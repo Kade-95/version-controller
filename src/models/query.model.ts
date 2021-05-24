fae9f36e394ba5be74fc2a2b81b927f60621d8f0
kennedy.ikeka <ikeka95@gmail.com>
2021-05-24 16:57:05 +0100

    Added LoneDB

--- src/models/query.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Base {
-    _id?: string;
-}
-
-export type LocalDocument<T> = {
-    [P in keyof (T & Base)]: (T & Base)[P]
-}
--- src/repo.database.ts
+++ src/repo.database.ts
@@ -1,10 +1,9 @@
-import { LocalDB } from "./local.storage";
-import { LocalDocument } from "./models/query.model";
+import { LoneDB, LoneDocument } from "lonedb";
 import { IRepo } from "./repo.class";
 
-const storage = new LocalDB<IRepo>("Repos");
+const storage = new LoneDB<IRepo>("Repos");
 
-export const readFunction = async (data: Partial<LocalDocument<IRepo>>) => {
+export const readFunction = async (data: Partial<LoneDocument<IRepo>>) => {
     return await storage.findOne(data);
 }
 
@@ -12,17 +11,17 @@ export const insertFunction = async (data: IRepo) => {
     return await storage.insertOne(data);
 }
 
-export const updateFunction = async (params: Partial<LocalDocument<IRepo>>, data: Partial<IRepo>) => {
+export const updateFunction = async (params: Partial<LoneDocument<IRepo>>, data: Partial<IRepo>) => {
     return await storage.updateOne(params, data);
 }
 
-export const deleteFunction = async (params: Partial<LocalDocument<IRepo>>) => {    
+export const deleteFunction = async (params: Partial<LoneDocument<IRepo>>) => {
     return await storage.deleteOne(params);
 }
 
 export interface RepoDatabase<T = any> {
-    insert: (data: IRepo<T>) => Promise<LocalDocument<IRepo<T>>>;
-    read: (data: Partial<LocalDocument<IRepo>>) => Promise<IRepo<T>>;
-    update: (params: Partial<LocalDocument<IRepo>>, data: Partial<IRepo>) => Promise<any>;
-    delete: (data: Partial<LocalDocument<IRepo>>) => Promise<IRepo<T>>;
+    insert: (data: IRepo<T>) => Promise<LoneDocument<IRepo<T>>>;
+    read: (data: Partial<LoneDocument<IRepo>>) => Promise<IRepo<T>>;
+    update: (params: Partial<LoneDocument<IRepo>>, data: Partial<IRepo>) => Promise<any>;
+    delete: (data: Partial<LoneDocument<IRepo>>) => Promise<IRepo<T>>;
 }
